000c522a843dfd65122ee9b7893b2e9084ed5354
Svante Rollenhagen <svante.rollenhagen@gmail.com>
2020-05-13 20:05:51 +0200

    fixed student network training (hopefully)

--- student_training.py
+++ student_training.py
@@ -1,13 +1,9 @@
-def train_student_model(student_model, teacher_model, X_t_train, y_t_train, X_t_val, y_t_val, X_s_train, hyperparams):
+def train_student_model(student_model, teacher_model,X_train, y_train, X_val, y_val, hyperparams):
   tf.keras.backend.clear_session()
   file_location="/content/drive/My Drive/DD2424Files/Results/" + filename() + "/"
-  optimizer_teacher = tf.keras.optimizers.Adam(learning_rate=hyperparams.get("learning_rate"), epsilon=hyperparams.get("epsilon"))
-  loss = tf.keras.losses.CategoricalCrossentropy()
   metrics = ['accuracy']
   s_model_string = "student_model"
   t_model_string = "teacher_model"
-  teacher_model.add(tf.keras.layers.Activation("softmax"))
-  teacher_model.compile(optimizer = optimizer_teacher, loss = loss, metrics = metrics)
 
   # Train teacher model
   BATCH_SIZE = hyperparams.get("batch_size")
@@ -18,10 +14,9 @@ def train_student_model(student_model, teacher_model, X_t_train, y_t_train, X_t_
       file_location + "models_" + t_model_string, monitor='val_accuracy', verbose=0, save_best_only=True,
       save_weights_only=False, mode='max', save_freq='epoch')
 
-  teacher_history = teacher_model.fit(X_t_train, y_t_train, batch_size=BATCH_SIZE, epochs=EPOCHS,
-                          validation_data=(X_t_val, y_t_val), verbose=1, callbacks = [save_callback])
   
-  y_s_train = teacher_model.predict(X_s_train)
-  print("shape of y_s_train: {} shape of y_t_train: {}".format(y_s_train.shape, y_t_train.shape))
-  student_model.compile(optimizer=optimizer_teacher, loss=tf.nn.softmax_cross_entropy_with_logits, metrics= [tf.keras.metrics.CategoricalCrossentropy()])
-  student_model.fit(X_s_train, y_s_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(X_t_val,y_t_val), verbose=1, callbacks = [save_callback])
+  y_train = teacher_model.predict(X_train)
+  print("shape of y_s_train: {} ".format(y_train.shape))
+  student_model.compile(optimizer="adam", loss=tf.keras.losses.MSE, metrics= [tf.keras.metrics.CategoricalCrossentropy(from_logits=True)])
+  history = student_model.fit(X_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(X_val,y_val), verbose=1, callbacks = [save_callback])
+  return student_model
